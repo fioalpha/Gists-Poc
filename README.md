2887af99bdfb9b2e57bade93bdab355ed859c664
fioalpha <fioalpha@gmail.com>
2021-05-15 22:46:09 -0300

    init project

---
+++ README.md
@@ -0,0 +1,5 @@
+# Gist Poc
+Nesse projeto testarei meus conhecimento como desenvolver
+Este projeto é bem simple aonde irei consumir a apis do `GIST`, 
+aonde retornar os mais recentes gist criado, precisarei fazer um lazy loader de GIST usando 
+a tecnica de loading infinite. 
---
+++ app/src/main/java/com/fioalpha/poc/gist_poc/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.fioalpha.poc.gist_poc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
